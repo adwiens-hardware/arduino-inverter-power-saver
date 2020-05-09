7661097a7779971d4e56db32abed4d315050d036
Andy Wiens <adwiens@gmail.com>
2020-05-08 21:38:24 -0500

    Faster updates & narrower swing

--- fridge-serial.ino
+++ fridge-serial.ino
@@ -1,7 +1,7 @@
 #define CPU_DIV (4) /* How many times slower the Arduino clone is than a normal one. */
 #define TEMPERATURE_PIN (A1) /* Pin the temperature sensor is connected to. */
-#define TARGET (766)
-#define SWING (2) /* 764 hottest, 768 coldest */
+#define TARGET (760.5)
+#define HALF_SWING (0.25) /* 760.25 hottest, 760.75 coldest */
 #define RELAY_PIN (2)
 
 enum states {
@@ -14,19 +14,21 @@ int state = FRIDGE_OFF;
 
 void setup() {
   pinMode(RELAY_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   analogReference(INTERNAL); // Analog range is 0-1.1 volts
   Serial.begin(38400);
-  avg = ((double)analogRead(TEMPERATURE_PIN)) / 4;
+  avg = ((double)analogRead(TEMPERATURE_PIN)) / 4.0;
 }
 
 void loop() {
-  avg = ((double)0.9995) * avg + ((double)0.0005) * ((double)analogRead(TEMPERATURE_PIN)) / 4;
+  avg = ((double)0.999) * avg + ((double)0.001) * ((double)analogRead(TEMPERATURE_PIN)) / 4.0;
   Serial.println(avg);
-  if (state == FRIDGE_ON && avg > TARGET + SWING) {
+  if (avg > TARGET + HALF_SWING) {
     state = FRIDGE_OFF;
-  } else if (state == FRIDGE_OFF && avg < TARGET - SWING) {
+  } else if (avg < TARGET - HALF_SWING) {
     state = FRIDGE_ON;
   }
   digitalWrite(RELAY_PIN, state);
+  digitalWrite(LED_BUILTIN, state);
   delay(1);
 }
